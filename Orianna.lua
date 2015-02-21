d17a78aead38aaefb53e1a9d38db2142f675dd1b
Sunts <nusenuse77@gmail.com>
2015-02-21 20:13:32 +0100

    Update Orianna.lua

--- Orianna.lua
+++ Orianna.lua
@@ -239,7 +239,7 @@ function OnLoad()
 	end
 	if VP.version == nil or type(tonumber(VP.version)) ~= "number" or tonumber(VP.version) < 3 then
 		print("Downloading the dependencies, please wait.")
-		DownloadFile('https://raw.github.com/Sunts/BoL/master/VPrediction.lua?rand=' .. math.random(1,10000), LIB_PATH..'vPrediction.lua', function () print('All files were downloaded successfully, F9 twice pls!') end)
+		DownloadFile('https://raw.github.com/SidaBoL/Chaos/master/VPrediction.lua?rand=' .. math.random(1,10000), LIB_PATH..'vPrediction.lua', function () print('All files were downloaded successfully, F9 twice pls!') end)
 		return
 	end
 	
